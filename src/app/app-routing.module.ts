fbdbc003e2d41740dd652be252a653ae58db8fcb
johnrepogit <john.repo.git@gmail.com>
2022-09-12 15:35:07 -0400

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { BorrowerComponent } from './borrower/borrower.component';
+import { OwnerComponent } from './owner/owner.component';
+import { LoguinComponent } from './loguin/loguin.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'user',component:UserComponent},
+  {path:'borrower',component:BorrowerComponent},
+  {path:'owner',component:OwnerComponent},
+  {path:'loguin',component:LoguinComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { HeaderComponent } from './header/header.component';
+import { BorrowerComponent } from './borrower/borrower.component';
+import { UserComponent } from './user/user.component';
+import { OwnerComponent } from './owner/owner.component';
+import { LoguinComponent } from './loguin/loguin.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    BorrowerComponent,
+    UserComponent,
+    OwnerComponent,
+    LoguinComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule,
+    MatCardModule,
+    MatFormFieldModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
